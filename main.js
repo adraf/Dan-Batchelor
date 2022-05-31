bd26528697dd263c03b7b0d9cce75b07ebe65bf2
Adam Rafferty <73663838+adraf@users.noreply.github.com>
2022-05-31 17:30:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -69,28 +69,3 @@ function stop() {
     }
     slides[slideIndex-1].style.display = "block";
   }
-
-
-//  Dropdown Button
-// var brgBtn = document.getElementsByClassName("smlbrgdropbtn");
-// var i;
-
-// for (i = 0; i < brgBtn.length; i++) {
-//   brgBtn[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.maxHeight){
-//       content.style.maxHeight = null;
-//     } else {
-//       content.style.maxHeight = content.scrollHeight + "px";
-//     } 
-//   })
-// }
-
-// listItems.forEach(item => {
-//   item.addEventListener("click", function() {
-//     brgBtn[0].classList.remove("active");
-//     var content = brgBtn[0].nextElementSibling;
-//     content.style.maxHeight = "";
-//   });
-// });
